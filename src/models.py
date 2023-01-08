12c20dfd4c2ee8cefd44396a5189a9e0e3a4ec32
Galdea <gabriel.aldea@gmail.com>
2023-01-08 21:47:28 +0000

    need to add session.commit() but need to generate database url with sqlite or similar first

--- src/models.py
+++ src/models.py
@@ -36,12 +36,13 @@ class Character(Base):
     id = Column(Integer, primary_key=True)
     mass = Column(Integer)
     height = Column(Integer)
-    hair_color: Column(String)
-    skin_color: Column(String)
-    eye_color: Column(String)
-    birth_year: Column(Integer)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
     species = Column(String)
     homeworld = Column(String)
+    gender = Column(String)
     name = Column(String)
 
 class Planet(Base):
@@ -56,6 +57,7 @@ class Planet(Base):
     terrain = Column(String)
     surface_water = Column(Integer)
     name = Column(String)
+    url = Column(String)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
@@ -66,3 +68,15 @@ engine = create_engine('sqlite:///starwars.db')
 # Create a session to manage the database
 Session = sessionmaker(bind=engine)
 session = Session()
+
+planet1 = Planet(climate="Arid", diameter=10465, gravity="1", name="Tatooine", orbital_period=304, population=120000, rotation_period=23, surface_water=1, terrain="Dessert", url="https://www.swapi.tech/api/planets/1")
+planet2 = Planet(diameter=12240, rotation_period=24, orbital_period=368, gravity="1 standard", population=1000000000000, climate="temperate", terrain="cityscape, mountains", surface_water="unknown", name="Coruscant", url="https://www.swapi.tech/api/planets/9")
+
+session.add(planet1)
+session.add(planet2)
+
+character1 = Character(birth_year="19 BBY", eye_color="Blue", gender="Male", hair_color="Blond", height=172, homeworld="https://www.swapi.tech/api/planets/1/", mass=77, name="Luke Skywalker", skin_color="Fair")
+character2 = Character(name="Leia Organa", height=150, mass=49, hair_color="brown", skin_color="light", eye_color="brown", birth_year="19BBY", gender="female", homeworld="https://www.swapi.tech/api/planets/2")
+
+session.add(character1)
+session.add(character2)
