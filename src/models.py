f7002e113d3cfd80d770b121b7918a87d2e4a5a9
Galdea <gabriel.aldea@gmail.com>
2023-01-08 21:24:22 +0000

    database is missing adding 4 models-2planets&2characters

--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 from sqlalchemy.sql.sqltypes import Enum
+from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,11 +16,6 @@ from sqlalchemy.sql.sqltypes import Enum
 
 Base = declarative_base()
 
-class Follower:
-    __tablename__ = 'Followers'
-    user_from_id = Column(Integer, ForeignKey('Users.id'))
-    user_to_id = Column(Integer, ForeignKey('Users.id'))
-
 class User(Base):
     __tablename__ = 'Users'
     id = Column(Integer, primary_key=True)  # Set primary key
@@ -27,24 +23,46 @@ class User(Base):
     lastname = Column(String)
     email = Column(String)
 
-class Post(Base):
-    __tablename__ = 'Post'
-    ID = Column(Integer, primary_key=True) # Add primary key
-    user_id = Column(Integer, ForeignKey('Users.id'))
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    user_id = Column(Integer, ForeignKey('Users.id'), primary_key=True)
+    character_id = Column(Integer, ForeignKey('Characters.id'), primary_key=True)
+    planet_id = Column(Integer, ForeignKey('Planets.id'), primary_key=True)
+    character = relationship("Character")
+    planet = relationship("Planet")
 
-class Comment(Base):
-    __tablename__ = 'Comment'
-    ID = Column(Integer, primary_key=True)  # Add primary key
-    post_id = Column(Integer, ForeignKey('Post.ID'))
-    author_id = Column(Integer, ForeignKey('Users.id'))
-    comment_text = Column(String)
+class Character(Base):
+    __tablename__ = 'Characters'
+    id = Column(Integer, primary_key=True)
+    mass = Column(Integer)
+    height = Column(Integer)
+    hair_color: Column(String)
+    skin_color: Column(String)
+    eye_color: Column(String)
+    birth_year: Column(Integer)
+    species = Column(String)
+    homeworld = Column(String)
+    name = Column(String)
 
-class Media(Base):
-    __tablename__ = 'Media'
-    ID = Column(Integer, primary_key=True)
-    type = Enum('active', 'inactive', 'pending')
-    url = Column(String)
-    post_id = Column(Integer, ForeignKey('Post.ID'))
+class Planet(Base):
+    __tablename__ = 'Planets'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String)
+    population = Column(Integer)
+    climate = Column(String)
+    terrain = Column(String)
+    surface_water = Column(Integer)
+    name = Column(String)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+# Create an engine to connect to the database
+engine = create_engine('sqlite:///starwars.db')
+
+# Create a session to manage the database
+Session = sessionmaker(bind=engine)
+session = Session()
